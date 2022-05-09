28605904a755b0ac92878c346b1d3ae5bfb2eac6
AYUSH SHARMA <96916029+Ayush123gg@users.noreply.github.com>
2022-05-10 05:13:43 +0530

    Time: 12 ms (95.32%), Space: 83.5 MB (21.25%) - LeetHub

---
+++ 164-maximum-gap/164-maximum-gap.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+        if(nums.length<2) return 0;
+        for(int num:nums){
+            if(min>num) min=num;
+            if(max<num) max=num;
+        }
+        int interval=(int)Math.ceil((max-min+0.0)/(nums.length-1));
+        int minbuckt[]=new int[nums.length-1];
+        int maxbuckt[]=new int[nums.length-1];
+        Arrays.fill(minbuckt,Integer.MAX_VALUE);
+        Arrays.fill(maxbuckt,Integer.MIN_VALUE);
+        for(int num:nums){
+            if(num==min||num==max) continue;
+            int idx=(num-min)/(interval);
+            minbuckt[idx]=Math.min(minbuckt[idx],num);
+            maxbuckt[idx]=Math.max(maxbuckt[idx],num);
+        }
+        int gap=0;
+        int prv=min;
+        for(int i=0;i<maxbuckt.length;i++){
+            if(maxbuckt[i]==Integer.MIN_VALUE) continue;
+            gap=Math.max(gap,minbuckt[i]-prv);
+            prv=maxbuckt[i];
+        }
+        gap=Math.max(gap,max-prv);
+        return gap;
+    }
+}
