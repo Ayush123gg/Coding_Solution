6477f761e8bd154736871a82eafb2611bf28c5ac
AYUSH SHARMA <96916029+Ayush123gg@users.noreply.github.com>
2022-05-11 02:08:07 +0530

    Time: 1 ms (82.95%), Space: 42.3 MB (92.56%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,4 +1,20 @@
 class Solution {
+    int binary(int num[],int i,int j,int key){
+        int indx=-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(num[mid]<=key){
+                j=mid-1;
+            }else{
+                if(mid<j&&num[mid]==num[mid+1]) i=mid+1;
+                else if(mid<j&&num[mid]>=num[mid+1]&&num[mid+1]>key) i=mid+1;
+                else{
+                    return mid;
+                }
+            }
+        }
+        return indx;
+    }
     public void nextPermutation(int[] nums) {
         int indx1=nums.length-2;
         while(indx1>=0&&nums[indx1]>=nums[indx1+1]) {
@@ -6,7 +22,8 @@ class Solution {
         }
         if(indx1>=0){
             int indx2=nums.length-1;
-            while(indx2>indx1&&nums[indx2]<=nums[indx1]) indx2--;
+            /*while(indx2>indx1&&nums[indx2]<=nums[indx1]) indx2--;*/
+            indx2=binary(nums,indx1+1,indx2,nums[indx1]);
             nums[indx1]=nums[indx1]+nums[indx2];
             nums[indx2]=nums[indx1]-nums[indx2];
             nums[indx1]=nums[indx1]-nums[indx2];
