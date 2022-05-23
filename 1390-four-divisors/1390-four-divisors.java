571b359abc4b93ace5c965a94135879ee38ec6df
AYUSH SHARMA <96916029+Ayush123gg@users.noreply.github.com>
2022-05-23 13:03:25 +0530

    Time: 92 ms (34.60%), Space: 47.7 MB (42.65%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,15 +1,19 @@
 class Solution {
     int calDivisors(int a){
         int sum=0,c=0;
+        if((int)Math.sqrt(a)*(int)Math.sqrt(a)==a)return 0; //perfect square root have odd divisor
         for(int i=1;i<=Math.sqrt(a);i++){
             if(a%i==0){
+                sum+=(i)+(a/i);
+                c+=2;
+              /*
                 sum+=(i);
                 if(a/i!=i){
                 sum+=(a/i);
                 c+=2;
                 }else{
                 c++;
-                }
+                }*/
             }
         }
         return c==4?sum:0;
