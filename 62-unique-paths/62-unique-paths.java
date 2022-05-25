3361ce0a3d24e8a472cd6b5d773b48f404b16845
AYUSH SHARMA <96916029+Ayush123gg@users.noreply.github.com>
2022-05-25 12:09:50 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (58.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,4 +1,5 @@
 class Solution {
+    // it takes quadratic times
     int dp(int arr[][],int i,int j,int a[][]){
         if(i>=arr.length||j>=arr[0].length){
             return 0;
@@ -9,11 +10,22 @@ class Solution {
         int r=dp(arr,i,j+1,a);
         return a[i][j]=l+r;
     }
+    // Combination method 
+    int Combination(int m,int n){
+        int total=m+n-2;
+        double res=1;
+        int r=m-1;
+        for(int i=1;i<=r;i++){
+            res=res*(total-r+i)/i;
+        }
+        return (int)res;
+    }
     public int uniquePaths(int m, int n) {
-        int arr[][]=new int[m][n];
+        /*int arr[][]=new int[m][n];
         int a[][]=new int[m+1][n+1];
         for(int[] aa:a)
-            Arrays.fill(aa,-1);
-        return dp(arr,0,0,a);
+            Arrays.fill(aa,-1);*/
+        
+        return Combination(m,n);
     }
 }
