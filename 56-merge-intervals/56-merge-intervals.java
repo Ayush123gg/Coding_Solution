54a45ceff21b0dd4a71f49a9c1a4d39b8241887b
AYUSH SHARMA <96916029+Ayush123gg@users.noreply.github.com>
2022-05-26 16:34:24 +0530

    Time: 18 ms (16.65%), Space: 55.8 MB (8.55%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int[][] merge(int[][] arr) {
-        List<List<Integer>> ar=new ArrayList<List<Integer>>();
-        Arrays.sort(arr,(a,b)->a[0]-b[0]);
-        int n=arr.length;
-        for(int i=0;i<n;){
-            int min=arr[i][0];
-            int max=arr[i][1];
-            if(i+1<n&&max>=arr[i+1][0]){
-                max=Math.max(arr[i+1][1],max);
-                int j=i+1;
-                while(j<n-1&&max>=arr[j+1][0]){
-                    max=Math.max(arr[j+1][1],max);
-                    j++;
-                }
-                i=j+1;
-            }else{
-                i++;
+    public int[][] merge(int[][] a) {
+        List<int[]> arr=new ArrayList<>();
+       /* if(a.length==0||a==null){
+            arr.toArray(new int[0][]);//empty 2d array
+        }*/
+        Arrays.sort(a,(x,y)-> x[0]-y[0]);
+        int s=a[0][0];
+        int l=a[0][1];
+        for(int []t:a){
+            if(t[0]<=l){
+                l=Math.max(l,t[1]);
             }
-            ArrayList<Integer> a=new ArrayList<Integer>();
-            a.add(min);
-            a.add(max);
-            ar.add(a);
+            else{
+                arr.add(new int[]{s,l});
+                s=t[0];
+                l=t[1];
+            }
         }
-        int aa[][]=new int[ar.size()][2];
-        for(int i=0;i<aa.length;i++){
-            aa[i][0]=ar.get(i).get(0);
-            aa[i][1]=ar.get(i).get(1);
+        arr.add(new int[]{s,l});
+       // return arr.toArray(int int[0][]);
+        int aa[][]=new int[arr.size()][];
+        int p=0;
+        for(int[] aaa:arr){
+            aa[p++]=aaa;
         }
         return aa;
     }
